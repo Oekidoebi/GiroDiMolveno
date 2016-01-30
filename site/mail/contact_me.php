813b11b2725cb88072e1b76c5c63360a88b99ac1
Oekidoebi <oekidoebi@gmail.com>
2016-01-30 14:13:52 +0100

    Opmaak webformulier

--- site/mail/contact_me.php
+++ site/mail/contact_me.php
@@ -23,6 +23,7 @@ $groepsgrootte = $_POST['groepsgrootte'];
 
 // Create the email and send the message
 $to = 'info@venividifietsie.com'; //  Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+//$to = 'van_ulzen@hotmail.com'; //  Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message\n\nAanbod: $aanbod\n\nAankomst: $dag - $maand - $jaar\n\nDagen: $dagen\n\nGroepsgrootte: $groepsgrootte";
 $headers = "From: noreply@venividifietsie.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
